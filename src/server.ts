7224fb6b2910c02203f5a2d9c7e04e76a94028af
Natalia Panfilenka <choco-cat@mail.ru>
2022-07-30 17:47:13 +0300

    fix: errors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 const app = require("./app.ts");
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
+app.listen(process.env["PORT"] || 3000, () =>
+  console.log(`App is running ${process.env["PORT"] || 3000}`)
 );
